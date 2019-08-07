9560e070092dee43477cf6e9a68b6c9689b4bf98
Hillary Spitzer <hspitzer2@gmail.com>
2019-08-07 23:21:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,10 @@ require 'open-uri'
 html = open("http://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-doc.css(".tout__label.heading.heading--level-4")
-
-courses = doc.css(".tout__label.heading.heading--level-4")
-
-courses.each do |course|
-  puts course.text.strip
-end
+# doc.css(".tout__label.heading.heading--level-4")
+# 
+# courses = doc.css(".tout__label.heading.heading--level-4")
+# 
+# courses.each do |course|
+#   puts course.text.strip
+# end
